4b2d3600f2b09020350b6d1915197d99957ab02a
ifTsuukiIsHere <ni.gomezh@duocuc.cl>
2024-09-08 04:12:52 -0300

    Estilos añadidos

--- src/app/paginas/register/register.module.ts
+++ src/app/paginas/register/register.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
-import { RegisterPageRoutingModule } from './register-routing.module';
-
 import { RegisterPage } from './register.page';
+import { RegisterPageRoutingModule } from './register-routing.module'; // Importa el módulo de enrutamiento específico de la página
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    RegisterPageRoutingModule
+    RegisterPageRoutingModule  // Asegúrate de que el módulo de enrutamiento esté importado
   ],
   declarations: [RegisterPage]
 })
--- src/app/paginas/register/register.page.ts
+++ src/app/paginas/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  registerForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+    }, {
+      validator: this.passwordMatchValidator('password', 'confirmPassword')
+    });
+  
+    console.log('Formulario inicializado:', this.registerForm);
   }
 
+  passwordMatchValidator(password: string, confirmPassword: string) {
+    return (formGroup: FormGroup) => {
+      const passControl = formGroup.controls[password];
+      const confirmPassControl = formGroup.controls[confirmPassword];
+
+      if (confirmPassControl.errors && !confirmPassControl.errors['mustMatch']) {
+        return;
+      }
+
+      if (passControl.value !== confirmPassControl.value) {
+        confirmPassControl.setErrors({ mustMatch: true });
+      } else {
+        confirmPassControl.setErrors(null);
+      }
+    };
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Formulario válido:', this.registerForm.value);
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
